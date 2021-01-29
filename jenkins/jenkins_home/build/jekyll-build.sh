fb14f7ccf7b4cab02734d6e1c3a9b97b757cd159
s-abe0 <shane.abe0@gmail.com>
2021-01-29 14:16:31 -0500

    use build with params

--- jenkins/jenkins_home/build/jekyll-build.sh
+++ jenkins/jenkins_home/build/jekyll-build.sh
@@ -4,5 +4,4 @@ echo "****************************"
 echo "*** BUILDING JEKYLL SITE ***"
 echo "****************************"
 
-WORKSPACE=/home/shane/coffeeintheclouds-cicd/jenkins/jenkins_home/workspace/coffeeintheclouds
-docker run --rm --volume="$WORKSPACE:/srv/jekyll" --volume="$WORKSPACE/vendor/bundle:/usr/local/bundle" -i jekyll/jekyll:3.8 jekyll build
+docker run --rm --volume="$1:/srv/jekyll" --volume="$1/vendor/bundle:/usr/local/bundle" -i jekyll/jekyll:3.8 jekyll build
